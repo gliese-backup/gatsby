51be99cdd7193d62c252b41346131d881e8aa2a5
gliese-backup <glieseb+1@proton.me>
2025-01-07 14:29:20 +0000

    Using Card Component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,27 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
 function Page() {
   return (
-    <div>
-      <h1>Gatsby</h1>
+    <div className="min-h-screen flex justify-center items-center">
+      <Card className="w-72 mx-auto ">
+        <CardHeader>
+          <CardTitle>Card Title</CardTitle>
+          <CardDescription>Card Description</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>Card Content</p>
+        </CardContent>
+        <CardFooter>
+          <p>Card Footer</p>
+        </CardFooter>
+      </Card>
     </div>
   );
 }
