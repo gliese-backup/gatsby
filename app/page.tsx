aba070be18e8d53271b0f0a9a28d917b90af2676
gliese-backup <glieseb+1@proton.me>
2025-01-09 06:11:29 +0000

    add <Header />

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,10 @@
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import Header from "@/components/Header";
 
 function Page() {
   return (
-    <div className="min-h-screen flex justify-center items-center">
-      <Card className="w-72 mx-auto ">
-        <CardHeader>
-          <CardTitle>Card Title</CardTitle>
-          <CardDescription>Card Description</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <p>Card Content</p>
-        </CardContent>
-        <CardFooter>
-          <p>Card Footer</p>
-        </CardFooter>
-      </Card>
-    </div>
+    <main>
+      <Header />
+    </main>
   );
 }
 
